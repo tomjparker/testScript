925246194482ebe5eb9cb0097800619bf8fa1601
Tom <112428963+tomjparker@users.noreply.github.com>
2024-02-13 11:03:44 +0000

    Add files via upload

---
+++ testScript.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Echo out a welcome message
+echo "Welcome to the bash script!"
+
+# Echo out some additional messages
+echo "This is a sample bash script."
+echo "It demonstrates echoing messages."
+echo "You can customize it as needed."
+
+# Echo out the current date and time
+echo "The current date and time is: $(date)"
